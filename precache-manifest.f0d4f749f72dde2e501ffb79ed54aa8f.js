c1540304ee10022f623dfd825539f1e95d3629d1
RajendraBhagroo <Raj@Fameria.com>
2018-12-16 06:32:23 -0500

    Updates

---
+++ precache-manifest.f0d4f749f72dde2e501ffb79ed54aa8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5fa496667db1df371272",
+    "url": "./static/js/main.5fa49666.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "./static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "5fa496667db1df371272",
+    "url": "./static/css/main.696ef16e.chunk.css"
+  },
+  {
+    "revision": "7b72b2696c659a9a3ed89e04b25994d1",
+    "url": "./index.html"
+  }
+];
