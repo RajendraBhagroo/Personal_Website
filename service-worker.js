652a24b9ed1badaa9bf8ca9537b23905d6f895e3
RajendraBhagroo <Raj@Fameria.com>
2018-12-18 04:55:46 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8c81fc731b2f368aa44d0e4c922e158e.js"
+  "./precache-manifest.389ec8c018f01a306995817a8296c4b9.js"
 );
 
 workbox.clientsClaim();
